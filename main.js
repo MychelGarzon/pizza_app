476ed8cc246d36ac293ea11962cd256e9df75d04
MychelGarzon <mtjomy17@gmail.com>
2023-09-11 22:04:56 +0300

    change in the display of the toppings

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const orderingPizza = () => {
   const orderingInfo = document.getElementById('orderingInfo');
   const name = document.querySelector('#name').value;
+  const deliveryInfo = document.getElementById('deliveryInfo');
+  const finalToppings = document.getElementById('finalToppings');
+
+  const totalPrice = document.getElementById('totalPrice');
   const pizzaSizeValue = document.querySelector(
     'input[name="pizza-size"]:checked'
   )?.value;
+
   const pizzaTopping = document.querySelectorAll(
     'input[name="topping"]:checked'
   );
+
   let count = 0;
   let extraTopping = 0;
-  if (pizzaTopping.length > 4) {
-    pizzaTopping.forEach((checkbox) => {
-      count++;
-    });
+  let selectedToppings = '';
+
+  pizzaTopping.forEach((input) => {
+    count++;
+    selectedToppings += input.value + ',';
+  });
+
+  if (count > 4) {
     extraTopping = (count - 4) * 0.5;
   }
 
@@ -22,13 +32,15 @@ const orderingPizza = () => {
   if (deliveryMethod === 'We will deliver your pizza within 1 hour') {
     deliveryCost = 5.0;
   }
-  if (name == '') {
-    text = 'Please type your name';
-  } else {
-    let total = parseFloat(pizzaSizeValue) + deliveryCost + extraTopping;
-    text = `Hello ${name} Your total order is ${total}€ ${deliveryMethod}
-    `;
-  }
+  let total = 0;
+  total = parseFloat(pizzaSizeValue) + deliveryCost + extraTopping;
+  let text = `HellO! ${name} 
+`;
 
   orderingInfo.textContent = text;
+  totalPrice.textContent = total;
+  deliveryInfo.textContent = deliveryMethod;
+  finalToppings.textContent = selectedToppings;
 };
+const pizzaForm = document.getElementById('pizza-form');
+pizzaForm.addEventListener('change', orderingPizza);
